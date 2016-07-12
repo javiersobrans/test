eb36fd00bb7be14d205283251274a22b31ec0853
xas <jsobrans@yahoo.es>
2016-07-12 21:30:32 +0200

    LITE DB

--- MvcTest/Controllers/HomeController.cs
+++ MvcTest/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcTest.Models;
 
 namespace MvcTest.Controllers
 {
@@ -13,5 +15,26 @@ namespace MvcTest.Controllers
         {
             return View();
         }
+
+        public ActionResult Lista()
+        {
+
+            List<Cliente> lista = new List<Cliente>();
+
+            using (var db = new LiteDatabase(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/OneData.db")))
+            {
+
+                var clientes = db.GetCollection<Cliente>("clientes");
+                var results = clientes.FindAll();
+                foreach (Cliente cli in results)
+                {
+                    lista.Add(cli);
+                }
+            }
+
+            return View(lista);
+        }
+
+
     }
 }
---
+++ MvcTest/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcTest.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        [Display(Name = "Teléfono movil")]
+        public string Telefono { get; set; }
+        public bool Activo { get; set; }
+        public DateTime Creado { get; set; }
+
+    }
+}
